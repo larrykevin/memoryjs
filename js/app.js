a042ead2730661e7893c08fd720b1c0d618b375f
Larry Caycho <larry.kevin.c.c@gmail.com>
2021-08-29 01:06:50 -0500

    Ajustando estilos de header

--- js/app.js
+++ js/app.js
@@ -4,9 +4,13 @@ const buttonLetsGo = document.querySelector('.lets-go');
 buttonLetsGo.addEventListener('click', function() {
     const welcome = document.querySelector('.welcome');
     const table = document.querySelector('.table');
+    const headerScore = document.querySelector('.header__score');
+    const headerInitial = document.querySelector('.header__initial');
 
     welcome.style.display = 'none';
     table.style.display = 'grid';
+    headerInitial.style.display = 'none';
+    headerScore.style.display = 'flex';
 })
 
 document.querySelectorAll('.card').forEach(function(elemento) {
